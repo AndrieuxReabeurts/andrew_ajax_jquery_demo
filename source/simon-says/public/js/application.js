f5a4bfcda12de6c4d15bcb1690dd8ddd61a0c9d1
Andrew <jdrewsdc@gmail.com>
2014-04-12 19:07:29 -0700

    click and get a color, maybe you'll get chartreuse

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
+  $('form').submit(function(event){
+    event.preventDefault();
+    console.log('submit')
 
+    $('#get_color').on('click', changeColor)
+
+    var changeColor = $.ajax({
+      type: 'POST',
+      data: $(this),
+      url: '/color',
+      success: function(data){
+        console.log(data)
+        $("#color_me li:nth-child(" + data.cell + ")").css("background-color", data.color);
+      },
+      fail: function(){
+        alert('fail')
+      }
+    });
+  });
 });
